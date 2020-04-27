a801a8f901ab7de6cff3b09df473b4de1617bc85
zoombulous <44047136+zoombulous@users.noreply.github.com>
2020-04-26 21:34:26 -0700

    faster random, shrank cards illegible, great speed

--- server/app.js
+++ server/app.js
@@ -4,6 +4,8 @@ const Socketio = require ("socket.io")(Http);
 var fs = require('fs');
 var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 var xhr = new XMLHttpRequest();
+var FastBitSet = require('fastbitset');
+
 
 var position = {
     x:200,
@@ -25,20 +27,65 @@ var position4 = {
     y:240
 };
 
-var z_broken_shuttle = base64_encode('./z_broken_shuttle.jpg');
+var z_broken_shuttle = base64_encode('./tiny_broken_shuttle.jpg');
                         
-var z_cruiser_patrol = base64_encode('./z_cruiser_patrol.jpg');
+var z_cruiser_patrol = base64_encode('./tiny_cruiser_patrol.jpg');
                        
-var z_engine_room = base64_encode('./z_engine_room.jpg');
+var z_engine_room = base64_encode('./tiny_engine_room.jpg');
                      
-var z_minor_tech_diff = base64_encode('./z_minor_tech_diff.jpg');
+var z_minor_tech_diff = base64_encode('./tiny_minor_tech_diff.jpg');
                          
-var z_the_big_black = base64_encode('./z_the_big_black.jpg')
+var z_the_big_black = base64_encode('./tiny_the_big_black.jpg')
 
 
 
 var travelCards = [
     [
+        z_broken_shuttle,
+        z_cruiser_patrol,
+        z_engine_room,
+        z_minor_tech_diff,
+        z_the_big_black,
+        z_broken_shuttle,
+        z_cruiser_patrol,
+        z_engine_room,
+        z_minor_tech_diff,
+        z_the_big_black,
+        z_broken_shuttle,
+        z_cruiser_patrol,
+        z_engine_room,
+        z_minor_tech_diff,
+        z_the_big_black,
+        z_broken_shuttle,
+        z_cruiser_patrol,
+        z_engine_room,
+        z_minor_tech_diff,
+        z_the_big_black,
+        z_broken_shuttle,
+        z_cruiser_patrol,
+        z_engine_room,
+        z_minor_tech_diff,
+        z_the_big_black,
+        z_broken_shuttle,
+        z_cruiser_patrol,
+        z_engine_room,
+        z_minor_tech_diff,
+        z_the_big_black,
+        z_broken_shuttle,
+        z_cruiser_patrol,
+        z_engine_room,
+        z_minor_tech_diff,
+        z_the_big_black,
+        z_broken_shuttle,
+        z_cruiser_patrol,
+        z_engine_room,
+        z_minor_tech_diff,
+        z_the_big_black,
+        z_broken_shuttle,
+        z_cruiser_patrol,
+        z_engine_room,
+        z_minor_tech_diff,
+        z_the_big_black,
         z_broken_shuttle,
         z_cruiser_patrol,
         z_engine_room,
@@ -82,6 +129,8 @@ function base64_decode(base64str, file) {
     console.log('File created from base64 encoded string');
 }
 
+
+
 Socketio.on("connection", socket => {
     socket.emit("position", position)
     socket.emit("position2", position2)
@@ -97,7 +146,9 @@ Socketio.on("connection", socket => {
     socket.on ("movingPicker", movingMove => {
         switch(movingMove) {
         case "alliance":
-            var chosenImage =
+
+            for (var i = 0; i < travelCards[0].length; i++)
+            var chosenImage =    
                 Math.floor(Math.random() *
                            travelCards[0].length);
             travelCards[1] =
@@ -117,7 +168,7 @@ Socketio.on("connection", socket => {
     socket.on ("picker", chosenMove => {
         switch(chosenMove) {
         case "alliance":
-            var chosenImage = Math.floor(Math.random() * images.length);
+            var chosenImage = Array.from(sampleF2(1, travelCards[0].length));
             chosenCard = images[chosenImage];
             var val = images[chosenImage];
             
@@ -270,3 +321,100 @@ Socketio.on("connection", socket => {
 Http.listen(3000, () => {
     console.log("Listening at :3000...");
 });
+
+
+//below this point is functions to help make the random pick faster
+
+
+// return an integer in [0,i)
+function randInt(i) {
+  return Math.floor(Math.random() * i);
+}
+
+// return m values in the range [0,n)
+function sampleS(m, n) {
+   let s = new Set();
+   while(s.size < m) {
+      s.add(randInt(n));
+   }
+   return s;
+}
+
+// Use the "mix" approach, which goes like this:
+//
+// 1) l2 is the smallest value such that 2^l2 >= n
+// 2) for each value x in the sequence 0..m-1 do this:
+// 3) hash x, using a function similar to splitmix64,
+//    which has no collision
+// 4) if the hash is >= n, hash again, until < n
+//
+// This approach doesn't need a (bit-) set,
+// as by design duplicates are avoided.
+// The disadvantage is that it's a custom PRNG.
+//
+// See also:
+// https://stackoverflow.com/questions/664014/what-integer-hash-function-are-good-that-accepts-an-integer-hash-key/12996028#12996028
+// http://xorshift.di.unimi.it/splitmix64.c
+//
+// At step 4, we could also skip the value - but
+// then the sequence can't be generated in parallel.
+
+function mix(m, n) {
+    m = m | 0;
+    n = n | 0;
+    var l2 = nextLog2(n) | 0;
+    var shift = (l2 >> 1) | 0;
+    var mask = ((1 << l2) - 1) | 0;
+    let s = new Int32Array(m);
+	  // var fbs = new FastBitSet();
+    for (let j = 0; (j|0) < m; j = (j+1)|0) {
+        s[j] = next(j, n, shift, mask);
+        // let x = fbs.checkedAdd(s[j]);
+        // assert(x == 1, "Bug in add at " + j);
+    }
+    return s;
+}
+
+function next(x, n, shift, mask) {
+    x = x | 0;
+    n = n | 0;
+    while (true) {
+        x = hash(x, shift, mask);
+        if ((x|0) < n) {
+            return x;
+        }
+    }
+}
+
+function nextLog2(x) {
+    x = x | 0;
+    let n = 1;
+    while ((1 << n) < x) {
+        n++;
+    }
+    return n | 0;
+}
+
+function hash(x, shift, mask) {
+    x = x | 0;
+    shift = shift | 0;
+    mask = mask | 0;
+    x = Math.imul((x >>> shift) ^ x, 0x45d9f3b);
+    x = x & mask;
+    x = Math.imul((x >>> shift) ^ x, 0x45d9f3b);
+    x &= mask;
+    x = x ^ (x >>> shift);
+    x &= mask;
+    return x | 0;
+}
+
+// return m values in the range [0,n)
+// attributed to Floyd
+function sampleF2(m, n) {
+    let s = new Set();
+    for (let j = n - m; j < n; j++) {
+        const t = randInt(j);
+        s.add( s.has(t) ? j : t );
+    }
+    return s;
+};
